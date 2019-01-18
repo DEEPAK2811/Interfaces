b3e81fa897f38bd8e3df357a48aa303a36384c68
Deepak Prakash <deepakprakash2811@gmai.com>
2019-01-18 23:43:04 +0530

    initial testing

---
+++ SPI_1Slave/SPI_1Slave/main.c
@@ -0,0 +1,18 @@
+/*
+ * SPI_1Slave.c
+ *
+ * Created: 18-01-2019 23:23:04
+ * Author : Deepak
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
